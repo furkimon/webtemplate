425f665dd38fc9b4a89f5158987e3074773e5284
furkimon <tonyalifurkan@gmail.com>
2020-10-28 08:15:19 +0300

    build

--- src/components/Touch.js
+++ src/components/Touch.js
@@ -1,19 +1,40 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import '../styles/Touch.css'
 
-export class Touch extends Component {
-    render() {
-        return (
-            <div className="touch__container" id="touch">
+
+const Touch = () => {
+    const [mailStyle, setMailStyle] = useState({})
+
+    const toggleHidden = () => {
+        if (mailStyle) {
+            setMailStyle({})
+        } else {
+            setMailStyle({
+                position: "absolute",
+                borderRadius: "0",
+                width: "100vw",
+                height: "100vh",
+                padding: "0",
+                overflow: "hidden"
+            })
+        }
+
+    }
+
+    return (
+        <div className="touch__container" id="touch">
+            <div className="touch__inner">
                 <div className="farewell">
                     <h1>Stay in <span>touch</span>.</h1>
                     <h3>I would like to hear from you.</h3>
                 </div>
-                <div className="mail">
+                <div className="mail" onClick={toggleHidden} style={mailStyle}>
+                    <span className="line line1"></span>
+                    <span className="line line2"></span>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default Touch
